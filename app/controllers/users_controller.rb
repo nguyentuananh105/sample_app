0994bf6c5c603728502b4d1f421a7c79a5c3963c
Nguyen Tuan Anh <nguyen.tuan.anh@framgia.com>
2013-10-01 10:41:10 +0700

    sing-in-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   # @user = User.new(name: "Foo Bar", email: "foo@invalid",
    #              password: "foo", password_confirmation: "bar")
     if @user.save
-    flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET '=no-te'" do
-    it "returns http success" do
-      get '=no-te'
-      response.should be_success
-    end
+  def new
+  	
+  end
+  def create
+  	
+  end
+  def destroy
+  	
   end
 
 end
