fcbef0dcb24cdbc86e5f19c1654f73a1f96594b6
Nguyen Tuan Anh <nguyen.tuan.anh@framgia.com>
2013-10-03 13:30:10 +0700

    sa

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003022005) do
+ActiveRecord::Schema.define(version: 20131003061823) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20131003022005) do
     t.datetime "updated_at"
   end
 
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
